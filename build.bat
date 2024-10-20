16dc7587b109b59d484fec7b29b8a474b77a1d47
lsmmega <pogmegalsm@outlook.com>
2024-10-20 22:15:20 +0800

    rename

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 bmp2nes gfx/sprites/weapons/rock_buster.bmp
 bmp2nes gfx/sprites/ready.bmp
 bmp2nes gfx/sprites/1up.bmp
-bmp2nes gfx/sprites/killed.bmp
+bmp2nes gfx/sprites/explosion.bmp
 bmp2nes gfx/sprites/bars/bar_vertical.bmp
 asm6 main.asm rm1.nes
 @pause
